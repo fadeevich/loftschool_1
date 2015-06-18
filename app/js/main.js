a3077ee06ab9c480876a07b79592c5f95c155c1c
Dmitry Fadeev <fadeevdima89@gmail.com>
2015-06-18 20:26:29 +0500

    add bpopup and qtip

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,105 @@
-$(document).ready(function() {
-	$('#project-add').click(function (event) {
-		event.preventDefault();
-		$('#overlay').fadeIn(200, 
-			function () {
-				$('#modal-window').css('display', 'block')
-													.animate({opacity: 1, top: '10%'}, 400);
-			});
-	});
-
-	$('#modal-window-btn-close, #overlay').click(function(event) {
-		event.preventDefault();
-		$('#modal-window').animate({opacity: 0, top: '-500px'}, 400,
-			function() {
-				$(this).css('display', 'none');
-				$('#overlay').fadeOut(200);
-			}
-		);
-	});
+$(function() {
+	Popup.init();
+	validation.init();
 });
+
+	$('input, textarea').placeholder();
+
+	var Popup = (function($) {
+
+    	function setUpListeners(){
+    		$('#project-add').on('click', function(e) {
+      	e.preventDefault();
+      	$('#modal-window').bPopup({
+      		modalClose: false,
+      		speed: 400,
+          transition: 'slideDown',
+	    		modalColor: '#58697a',
+	    		opacity: 0.75
+      	});
+			});
+		}
+		return {
+			init: function(){
+				setUpListeners();
+			}
+		}
+	}(jQuery));
+	
+var validation = (function ($) {
+	
+		function setUpListeners(){
+			$('form').on('submit', validateForm);
+			$('#btn-reset, .b-close').on('click', hideQTip);
+		}
+
+		function validateForm(){
+			var form = $('form');
+					inputs = form.find('.modal-window-input, .modal-window-textarea, .contact-me-input, .contact-me-textarea, .login-form-input');
+					validation = true;
+
+			$.each(inputs, function(index, value) {
+				var input = $(value),
+						inputValue = input.val(),
+						position = input.attr('tooltip-position');
+
+				if (position === "right") {
+					position_tooltip = {
+						my: 'center left', 
+						at: 'center right'
+					}
+				} else {
+					position_tooltip = {
+						my: 'center right', 
+						at: 'center left'
+					}
+				};
+
+				if (inputValue.length === 0) {
+					showQTip(input,position);
+					validation = false;
+				}
+			});
+
+			return validation;
+		}
+
+		function hideQTip(){
+			inputs.trigger('hideToolTips');
+		}
+
+		function showQTip(input,position) {
+			input.qtip({
+				content: {
+        text: function(event, api) {
+          return $(this).attr('tooltip-content');
+        }
+    },
+				position: position_tooltip,
+				style: {
+					classes: 'tooltip',
+					tip: {
+					    corner: true,
+					    border: 0,
+					    width: 10
+					}
+				},
+				show:{
+					event: 'show'
+				},
+				hide:{
+					event: 'keydown hideToolTips'
+				},
+				events:{
+					show: function (event, api) {input.addClass('has-error');},
+					hide: function (event, api) {input.removeClass('has-error');}
+				}
+
+			}).trigger('show');	
+		}
+		return {
+			init: function(){
+				setUpListeners();
+			}
+		}
+	}(jQuery));
